3c148cead5616b8116748417be39ef9aa81ba114
izikgo <izikgo@st.technion.ac.il@ml5.iem.technion.ac.il>
2016-12-05 18:49:02 +0200

    best model including dropout, and plots

--- HW1/classification_mnist.lua
+++ HW1/classification_mnist.lua
@@ -57,9 +57,17 @@ model:add(nn.SpatialMaxPooling(2, 2, 2, 2)) -- 64x14x14 in, 64x7x7 out
 
 model:add(nn.View(64 * 7 * 7)) -- reshapes the image into a vector without copy
 
--- Softmax layer
-model:add(nn.Linear(64 * 7 * 7, 10))
-model:add(nn.LogSoftMax())
+-- Dropout layer
+model:add(nn.Dropout(0.4))
+
+-- FC layer I
+model:add(nn.Linear(64 * 7 * 7, 14))
+
+-- Dropout layer
+model:add(nn.Dropout(0.4))
+
+-- FC layer II
+model:add(nn.Linear(14, 10))
 
 localize(model)
 
@@ -70,43 +78,37 @@ end
 print(tostring(model))
 
 local w, dE_dw = model:getParameters()
-print('Number of parameters:', w:nElement()) --over-specified model
+print('Number of parameters:', w:nElement())
 
 
----- ### Classification criterion
-
-criterion = localize(nn.ClassNLLCriterion())
-
----	 ### predefined constants
+criterion = localize(nn.CrossEntropyCriterion())
 
 
 batchSize = 128
-
 optimState = {
-    learningRate = 0.1
-    
+    learningRate = 0.01,
 }
 
 --- ### Main evaluation + training function
 
 function forwardNet(data, labels, train)
-	timer = torch.Timer()
+    timer = torch.Timer()
 
     --another helpful function of optim is ConfusionMatrix
     local confusion = optim.ConfusionMatrix(torch.range(0,9):totable())
     local lossAcc = 0
-    local numBatches = 0
     if train then
-        --set network into training mode
         model:training()
+    else 
+        model:evaluate()
     end
-    for i = 1, data:size(1) - batchSize, batchSize do
-        numBatches = numBatches + 1
-        local x = data:narrow(1, i, batchSize)
-        local yt = labels:narrow(1, i, batchSize)
+    for i = 1, data:size(1), batchSize do
+        local curBatchSize = math.min(batchSize, data:size(1) - i + 1)
+        local x = data:narrow(1, i, curBatchSize)
+        local yt = labels:narrow(1, i, curBatchSize)
         local y = model:forward(x)
         local err = criterion:forward(y, yt)
-        lossAcc = lossAcc + err
+        lossAcc = lossAcc + err*curBatchSize
         confusion:batchAdd(y,yt)
         
         if train then
@@ -123,9 +125,9 @@ function forwardNet(data, labels, train)
     end
     
     confusion:updateValids()
-    local avgLoss = lossAcc / numBatches
+    local avgLoss = lossAcc / data:size(1)
     local avgError = 1 - confusion.totalValid
-	print(timer:time().real .. ' seconds')
+    -- print(timer:time().real .. ' seconds')
 
     return avgLoss, avgError, tostring(confusion)
 end
@@ -133,87 +135,36 @@ end
 
 --- ### Train the network on training set, evaluate on separate set
 
-
-epochs = 50
+epochs = 1000
 
 trainLoss = torch.Tensor(epochs)
 testLoss = torch.Tensor(epochs)
 trainError = torch.Tensor(epochs)
 testError = torch.Tensor(epochs)
 
---reset net weights
-model:apply(function(l) l:reset() end)
-
 for e = 1, epochs do
     trainData, trainLabels = shuffle(trainData, trainLabels) --shuffle training data
-    trainLoss[e], trainError[e] = forwardNet(trainData, trainLabels, true)
-    testLoss[e], testError[e], confusion = forwardNet(testData, testLabels, false)
+    forwardNet(trainData, trainLabels, true) -- train the network
+    trainLoss[e], trainError[e] = forwardNet(trainData, trainLabels, false) -- evaluate on train
+    testLoss[e], testError[e], confusion = forwardNet(testData, testLabels, false) -- evaluate on test
     
-    if e % 5 == 0 then
+    if e % 10 == 0 then
         print('Epoch ' .. e .. ':')
         print('Training error: ' .. trainError[e], 'Training Loss: ' .. trainLoss[e])
         print('Test error: ' .. testError[e], 'Test Loss: ' .. testLoss[e])
         print(confusion)
-    end
+        print('########################################################################')
+   end
 end
-
-
-
----		### Introduce momentum, L2 regularization
---reset net weights
---[[
-model:apply(function(l) l:reset() end)
-
-optimState = {
-    learningRate = 0.1,
-    momentum = 0.9,
-    weightDecay = 1e-3
-    
-}
-for e = 1, epochs do
-    trainData, trainLabels = shuffle(trainData, trainLabels) --shuffle training data
-    trainLoss[e], trainError[e] = forwardNet(trainData, trainLabels, true)
-    testLoss[e], testError[e], confusion = forwardNet(testData, testLabels, false)
-end
-
-print('Training error: ' .. trainError[epochs], 'Training Loss: ' .. trainLoss[epochs])
-print('Test error: ' .. testError[epochs], 'Test Loss: ' .. testLoss[epochs])
-]]
-
-
-
-
---- ### Insert a Dropout layer
---[[
-model:insert(nn.Dropout(0.9):cuda(), 8)
-
-
-]]
-
-
-
 
 -- ********************* Plots *********************
---[[
+
 require 'gnuplot'
 local range = torch.range(1, epochs)
 gnuplot.pngfigure('test.png')
+gnuplot.logscale(true)
 gnuplot.plot({'trainLoss',trainLoss},{'testLoss',testLoss})
 gnuplot.xlabel('epochs')
 gnuplot.ylabel('Loss')
 gnuplot.plotflush()
-]]
-
-
-
-
-
-
-
-
-
-
-
-
-
 
